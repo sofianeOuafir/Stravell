34840110cabd572f6a0eb573b9e173d6d562f821
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2018-12-27 15:18:20 -0500

    remove enforce HTTPS

--- server/server.js
+++ server/server.js
@@ -3,16 +3,13 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
-var enforce = require('express-sslify');
-var http = require('http');
 
-app.use(enforce.HTTPS());
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-http.createServer(app).listen(port, function() {
-  console.log('Express server listening on port ' + port);
+app.listen(port, () => {
+  console.log('Server is up!');
 });
