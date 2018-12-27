58be17186a2dc9a6ec4c465e59fc56e7443f117c
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2018-12-27 14:57:28 -0500

    enforce HTTPS

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
+var enforce = require('express-sslify');
 
 app.use(express.static(publicPath));
+app.use(enforce.HTTPS());
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
