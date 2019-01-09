d2abcab1ac0cd478a76889f544a9007c79cac24b
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2019-01-09 14:40:37 -0500

    make editor bigger when creating/editing a post

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,20 +1,23 @@
 import React from "react";
-
+import uuid from 'uuid';
 class Checkbox extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    const id = uuid();
     return (
       <div>
         <input
+          id={id}
+          name={id}
           className="mr1"
           onChange={this.props.handleChange}
           type="checkbox"
           checked={this.props.checked}
         />
-        <label>{ this.props.label }</label>
+        <label htmlFor={id}>{ this.props.label }</label>
       </div>
     );
   }
