906c52d5a1a2c9adb28bd6696207a249f5a9d08d
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2019-01-22 14:25:20 -0500

    wip

--- server/server.js
+++ server/server.js
@@ -1,25 +1,3 @@
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const next = require('next')
-
-// const port = parseInt(process.env.PORT, 10) || 3000
-// const dev = process.env.NODE_ENV !== 'production'
-// const app = next({ dev })
-// const handle = app.getRequestHandler()
-
-// app.prepare().then(() => {
-//   const server = express();
-//   server.use(bodyParser.json());
-//   server.get('*', (req, res) => {
-//     return handle(req, res)
-//   });
-
-//   server.listen(port, err => {
-//     if (err) throw err
-//     console.log(`> Ready on port ${port}`)
-//   })
-// })
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const session = require('express-session')
