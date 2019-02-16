65e417ea29067d9e9b690ba85bb5d86b1cb72956
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2019-02-15 22:27:52 -0500

    wip

---
+++ src/components/SocialShareButtons.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FacebookShareButton } from 'react-share';
+
+const SocialShareButtons = () => {
+  <div>
+  <FacebookShareButton></FacebookShareButton>
+  </div>
+}
+
+export default SocialShareButtons
