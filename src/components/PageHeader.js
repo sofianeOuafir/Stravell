a727eddb5e7fda05ba0f5e6d0c128a6663bbcfaa
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2019-02-18 11:12:06 -0500

    wip

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SocialShareButtons from './SocialShareButtons';
 
 const PageHeader = ({ className, children, title }) => (
   <div className={className ? className : "page-header"}>
     <div className="content-container">
       {children ? children : <h1 className="page-header__title">{title}</h1>}
+      <SocialShareButtons />
     </div>
   </div>
 );
--- src/components/SocialShareButtons.js
+++ src/components/SocialShareButtons.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import { FacebookShareButton } from 'react-share';
+import React from "react";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon
+} from "react-share";
 
-const SocialShareButtons = () => {
+const SocialShareButtons = () => (
   <div>
-  <FacebookShareButton></FacebookShareButton>
+    <FacebookShareButton url="https://stravell.com">
+      <FacebookIcon />
+    </FacebookShareButton>
+    <TwitterShareButton url="https://stravell.com">
+      <TwitterIcon />
+    </TwitterShareButton>
   </div>
-}
+);
 
-export default SocialShareButtons
+export default SocialShareButtons;
