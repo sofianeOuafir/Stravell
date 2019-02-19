d293dfe7913ca12f77aa732d99173c0d7bf08cb3
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2019-02-19 11:08:56 -0500

    add google site verification tag

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/png" href="/static/images/favicon.png" />  
+        <meta name="google-site-verification" content="_3GsWLdx2YWaGidwUcPqulfw4iEC1Vvh88fO9vZuzz0" />
         <script type="text/javascript" src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_PLACES_API_KEY}&libraries=places`}></script>
       </Head>
         <body className="custom_class">
