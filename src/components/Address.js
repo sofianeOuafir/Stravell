4e31bf77d5dfbe453299328ddd235d8ed3cbb949
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2019-02-20 22:29:39 -0500

    add as path in address component

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,20 +1,24 @@
 import React, { Fragment } from "react";
 import { MdMyLocation } from "react-icons/md";
 import Link from "next/link";
+import { slugify } from "underscore.string";
 
 import { getPlaceIdFromLatLng } from "./../lib/utils/place";
 
-const Address = ({ iconClassName, addressClassName, address, lat, lng }) => (
-  <div className="flex align-items--center">
-    <Link href={`/place?id=${getPlaceIdFromLatLng({ lat, lng })}`}>
-      <a className="no-text-decoration text-dark-grey">
-        <MdMyLocation className={iconClassName ? iconClassName : ""} />
-        <span className={addressClassName ? addressClassName : ""}>
-          {address}
-        </span>
-      </a>
-    </Link>
-  </div>
-);
+const Address = ({ iconClassName, addressClassName, address, lat, lng }) => {
+  const placeId = getPlaceIdFromLatLng({ lat, lng });
+  return (
+    <div className="flex align-items--center">
+      <Link href={`/place?id=${placeId}`} as={`/place/${slugify(address)}/${placeId}`}>
+        <a className="no-text-decoration text-dark-grey">
+          <MdMyLocation className={iconClassName ? iconClassName : ""} />
+          <span className={addressClassName ? addressClassName : ""}>
+            {address}
+          </span>
+        </a>
+      </Link>
+    </div>
+  );
+};
 
 export default Address;
